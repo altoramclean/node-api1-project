287f9bcb73ab04229c730f184f7f0881b97c4ce3
Altora Mclean <altoramclean@gmail.com>
2022-01-10 19:55:01 -0500

    done

--- api/server.js
+++ api/server.js
@@ -67,13 +67,55 @@ server.get("/api/users/:id", ( req, res ) => {
 
 
 // DELETE	/api/users/:id	
-server.delete("/api/users/:id", ( req, res ) => {
-    
+server.delete("/api/users/:id", async( req, res ) => {
+    try{
+    const possibleUser = await User.findById(req.params.id)
+    if (!possibleUser) {
+        res.status(404).json({
+            message: 'The user with the specified ID does not exist',
+        })
+    } else {
+        const deletedUser = await User.remove(possibleUser.id)
+        res.status(200).json(deletedUser)
+    }
+} catch(err) {
+    res.status(500).json({
+        message: 'error deleting user',
+        err: err.message,
+        stack: err.stack,
+    })
+}
 })
+
 
 // PUT	/api/users/:id	
 server.put("/api/users/:id", async ( req, res ) => {
-    
+    try{
+        const possibleUser = await User.findById(req.params.id)
+        if (!possibleUser){
+        res.status(404).json({
+            message: 'The user with the specified ID does not exist',
+        })
+        } else {
+            if(!req.body.name || !req.body.bio){
+                res.status(400).json({
+                message: 'Please provide name and bio for the user',
+                })
+            } else {
+            const updatedUser = await User.update(
+                req.params.id,
+                req.body,
+            )
+            res.status(200).json(updatedUser)
+            }
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: 'error updating user',
+            err: err.message,
+            stack: err.stack,
+        })
+    }
 })
 
 
